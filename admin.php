54c9c344f62b0ddb8823401bb5c3ebc062de3b09
Tiago B <tiago.gpes@gmail.com>
2019-02-05 18:34:50 -0200

    Adição do link do cadastro de fornecedores.

---
+++ admin.php
@@ -0,0 +1,26 @@
+<?php
+    require 'lojacontroller.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>PHP Store - Admin</title>
+    </head>
+    <body>
+       <h1 style="text-align: center">Pagina Admin</h1> 
+       <h3><a href="index.php">Pagina Inicial</a></h3>
+       <h2>Cadastros</h2>
+       <ul style="list-style-type:none;">
+            <li><a href="">Produtos</a></li>            
+            <li><a href="cadfornecedor.php">Fornecedores</a></li>
+            <li><a href="cadmarca.php">Marca</a></li>
+            <li><a href="cadsecao.php">Seçao</a></li>
+            <li><a href="">Visualizar informaçoes</a></li>
+       </ul>
+
+    </body>
+</html>
