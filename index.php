8a9fc73a177d7aec8f6494149a65265450eb0c77
Tiago B <tiago.gpes@gmail.com>
2019-02-05 18:36:10 -0200

    adição do link para a página administrativa

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>PHP STORE - Pagina Inicial</title>
+    </head>
+    <body>
+        <h1 style="text-align: center;">Pagina Inicial</h1>            
+        <h3><a href="admin.php">Admin</a></h3>
+    </body>
+</html> 
